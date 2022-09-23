2429ff571b2bb84541a39ba3e95af0de286bdf99
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-09-23 22:47:34 +0600

    header section added

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -1,12 +1,15 @@
 
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to my Rest Countries</h1>
-            <nav>
-                
+            <nav className='header'>
+                <a href="/home">Home</a>
+                <a href="/countries">Countries</a>
+                <a href="/about">About</a>
             </nav>
         </div>
     );
