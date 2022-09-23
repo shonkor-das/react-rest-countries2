56bad9df24181f548824018bf6a8201f6f974f31
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-09-23 22:38:22 +0600

    footer section added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Countries from './conponents/Countries/Countries';
 import Country from './conponents/Country/Country';
 import Footer from './conponents/Footer/Footer';
+import Header from './conponents/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Countries></Countries>
       {/* <Country></Country> */}
       <Footer></Footer>
---
+++ src/conponents/Header/Header.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to my Rest Countries</h1>
+            <nav>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
