d5855ec14c6169fe94b97cec521dcad632fca5db
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-09-23 21:40:37 +0600

    footer section added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Countries from './conponents/Countries/Countries';
 import Country from './conponents/Country/Country';
+import Footer from './conponents/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Countries></Countries>
-      <Country></Country>
+      {/* <Country></Country> */}
+      <Footer></Footer>
     </div>
   );
 }
--- src/conponents/Countries/Countries.js
+++ src/conponents/Countries/Countries.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css';
+import Footer from '../Footer/Footer';
 
 const Countries = () => {
     const [Countries, setCountries ] = useState([])
@@ -17,20 +18,21 @@ const Countries = () => {
     return (
         <div>
             <h1>Hello from Countries: {Countries.length}</h1>
-        <div className='countries-container'>
-            {
-                Countries.map(country => <Country 
-                    key={country.cca3}
-                    name={country.name.common} 
-                    population={country.population}
-                    region ={country.region}
-                    area={country.area}
-                    flags={country.flags.png}>
-                    </Country>)
-            }
-        </div>
+            <div className='countries-container'>
+                {
+                    Countries.map(country => <Country 
+                        key={country.cca3}
+                        name={country.name.common} 
+                        population={country.population}
+                        region ={country.region}
+                        area={country.area}
+                        flags={country.flags.png}>
+                        </Country>)
+                }
+            </div>
         </div>
     );
 };
+
 
 export default Countries;
